2b7995d4a646b28f0c7dbde9cb46ec5015d088ba
Sebastian Paarmann <git@s-paarmann.de>
2023-10-10 16:06:32 +0200

    Start updating Chomp for the bit variable changes

--- Chomp.thy
+++ Chomp.thy
@@ -1,55 +1,62 @@
 theory Chomp imports Syntax begin
 
-nominal_function "chomp\<^sub>1\<^sub>e" :: "exp \<Rightarrow> var \<Rightarrow> nat \<Rightarrow> exp" where
-  "chomp\<^sub>1\<^sub>e (Len x PktIn) y _ = (if x = y then Plus (Num 1) (Len x PktIn) else (Len x PktIn))" |
-  "chomp\<^sub>1\<^sub>e (Packet x PktIn) y n = (if x = y then Concat (Bv [BitVar n]) (Packet x PktIn)
+nominal_function "chomp\<^sub>1\<^sub>e" :: "exp \<Rightarrow> var \<Rightarrow> exp" where
+  "chomp\<^sub>1\<^sub>e (Len x PktIn) y = (if x = y then Plus (Num 1) (Len x PktIn) else (Len x PktIn))" |
+  "chomp\<^sub>1\<^sub>e (Packet x PktIn) y = (if x = y then Concat (Bv [BitVar]) (Packet x PktIn)
                                            else (Packet x PktIn))" |
-  "chomp\<^sub>1\<^sub>e (Slice (SlPacket x PktIn) l r) y n = (if x \<noteq> y then (Slice (SlPacket x PktIn) l r)
-    else (if r \<le> 1 then (Bv [BitVar n])
-    else (if l = 0 then (Concat (Bv [BitVar n]) (Slice (SlPacket x PktIn) 0 (r - 1)))
+  "chomp\<^sub>1\<^sub>e (Slice (SlPacket x PktIn) l r) y = (if x \<noteq> y then (Slice (SlPacket x PktIn) l r)
+    else (if r \<le> 1 then (Bv [BitVar])
+    else (if l = 0 then (Concat (Bv [BitVar]) (Slice (SlPacket x PktIn) 0 (r - 1)))
     else (Slice (SlPacket x PktIn) (l - 1) (r - 1)))))" |
-  "chomp\<^sub>1\<^sub>e (Plus e\<^sub>1 e\<^sub>2) y n = Plus (chomp\<^sub>1\<^sub>e e\<^sub>1 y n) (chomp\<^sub>1\<^sub>e e\<^sub>2 y n)" |
-  "chomp\<^sub>1\<^sub>e (Concat e\<^sub>1 e\<^sub>2) y n = Concat (chomp\<^sub>1\<^sub>e e\<^sub>1 y n) (chomp\<^sub>1\<^sub>e e\<^sub>2 y n)" |
-  "chomp\<^sub>1\<^sub>e e _ _ = e"
+  "chomp\<^sub>1\<^sub>e (Plus e\<^sub>1 e\<^sub>2) y = Plus (chomp\<^sub>1\<^sub>e e\<^sub>1 y) (chomp\<^sub>1\<^sub>e e\<^sub>2 y)" |
+  "chomp\<^sub>1\<^sub>e (Concat e\<^sub>1 e\<^sub>2) y = Concat (chomp\<^sub>1\<^sub>e e\<^sub>1 y) (chomp\<^sub>1\<^sub>e e\<^sub>2 y)" |
+  "chomp\<^sub>1\<^sub>e e _ = e"
   sorry
 nominal_termination (eqvt)
   by lexicographic_order
 
-nominal_function chomp\<^sub>1\<^sub>\<phi> :: "formula \<Rightarrow> var \<Rightarrow> nat \<Rightarrow> formula" where
-  "chomp\<^sub>1\<^sub>\<phi> (Eq e\<^sub>1 e\<^sub>2) x n = Eq (chomp\<^sub>1\<^sub>e e\<^sub>1 x n) (chomp\<^sub>1\<^sub>e e\<^sub>2 x n)" |
-  "chomp\<^sub>1\<^sub>\<phi> (Gt e\<^sub>1 e\<^sub>2) x n = Gt (chomp\<^sub>1\<^sub>e e\<^sub>1 x n) (chomp\<^sub>1\<^sub>e e\<^sub>2 x n)" |
-  "chomp\<^sub>1\<^sub>\<phi> (And \<phi>\<^sub>1 \<phi>\<^sub>2) x n = And (chomp\<^sub>1\<^sub>\<phi> \<phi>\<^sub>1 x n) (chomp\<^sub>1\<^sub>\<phi> \<phi>\<^sub>2 x n)" |
-  "chomp\<^sub>1\<^sub>\<phi> (Not \<phi>) x n = Not (chomp\<^sub>1\<^sub>\<phi> \<phi> x n)" |
-  "chomp\<^sub>1\<^sub>\<phi> \<phi> _ _ = \<phi>"
+nominal_function chomp\<^sub>1\<^sub>\<phi> :: "formula \<Rightarrow> var \<Rightarrow> formula" where
+  "chomp\<^sub>1\<^sub>\<phi> (Eq e\<^sub>1 e\<^sub>2) x = Eq (chomp\<^sub>1\<^sub>e e\<^sub>1 x) (chomp\<^sub>1\<^sub>e e\<^sub>2 x)" |
+  "chomp\<^sub>1\<^sub>\<phi> (Gt e\<^sub>1 e\<^sub>2) x = Gt (chomp\<^sub>1\<^sub>e e\<^sub>1 x) (chomp\<^sub>1\<^sub>e e\<^sub>2 x)" |
+  "chomp\<^sub>1\<^sub>\<phi> (And \<phi>\<^sub>1 \<phi>\<^sub>2) x = And (chomp\<^sub>1\<^sub>\<phi> \<phi>\<^sub>1 x) (chomp\<^sub>1\<^sub>\<phi> \<phi>\<^sub>2 x)" |
+  "chomp\<^sub>1\<^sub>\<phi> (Not \<phi>) x = Not (chomp\<^sub>1\<^sub>\<phi> \<phi> x)" |
+  "chomp\<^sub>1\<^sub>\<phi> \<phi> _ = \<phi>"
   sorry
 nominal_termination (eqvt)
   by lexicographic_order
 
 (* TODO: Is `x` as both the chomp arg and the Sigma var correct here? Same for Refinement.
    I think the impl *doesn't* keep them the same?*)
-nominal_function chompRef\<^sub>1 :: "heap_ty \<Rightarrow> var \<Rightarrow> nat \<Rightarrow> heap_ty" where
-  "chompRef\<^sub>1 (Sigma x \<tau>\<^sub>1 \<tau>\<^sub>2) x n = Sigma x (chompRef\<^sub>1 \<tau>\<^sub>1 x n) (chompRef\<^sub>1 \<tau>\<^sub>2 x n)" |
-  "chompRef\<^sub>1 (Choice \<tau>\<^sub>1 \<tau>\<^sub>2) x n = Choice (chompRef\<^sub>1 \<tau>\<^sub>1 x n) (chompRef\<^sub>1 \<tau>\<^sub>2 x n)" |
-  "chompRef\<^sub>1 (Refinement x \<tau> \<phi>) x n = Refinement x (chompRef\<^sub>1 \<tau> x n) (chomp\<^sub>1\<^sub>\<phi> \<phi> x n)" |
-  "chompRef\<^sub>1 (Substitution \<tau>\<^sub>1 y \<tau>\<^sub>2) x n = Substitution (chompRef\<^sub>1 \<tau>\<^sub>1 x n) y (chompRef\<^sub>1 \<tau>\<^sub>2 x n)" |
-  "chompRef\<^sub>1 \<tau> _ _ = \<tau>"
+nominal_function chompRef\<^sub>1 :: "heap_ty \<Rightarrow> var \<Rightarrow> heap_ty" where
+  "atom y \<sharp> x \<Longrightarrow> chompRef\<^sub>1 (Sigma y \<tau>\<^sub>1 \<tau>\<^sub>2) x = Sigma y (chompRef\<^sub>1 \<tau>\<^sub>1 x) (chompRef\<^sub>1 \<tau>\<^sub>2 x)" |
+  "chompRef\<^sub>1 (Choice \<tau>\<^sub>1 \<tau>\<^sub>2) x = Choice (chompRef\<^sub>1 \<tau>\<^sub>1 x) (chompRef\<^sub>1 \<tau>\<^sub>2 x)" |
+  "atom y \<sharp> x \<Longrightarrow> chompRef\<^sub>1 (Refinement y \<tau> \<phi>) x = Refinement y (chompRef\<^sub>1 \<tau> x) (chomp\<^sub>1\<^sub>\<phi> \<phi> x)" |
+  "atom y \<sharp> x \<Longrightarrow> chompRef\<^sub>1 (Substitution \<tau>\<^sub>1 y \<tau>\<^sub>2) x = Substitution (chompRef\<^sub>1 \<tau>\<^sub>1 x) y (chompRef\<^sub>1 \<tau>\<^sub>2 x)" |
+  "chompRef\<^sub>1 \<tau> _ = \<tau>"
   sorry
 nominal_termination (eqvt)
   by lexicographic_order
 
-(* TODO: Does having Refinement y in the first def here have the same problem as heap_ty_empty etc.?*)
-(* Maybe it should just have a freshness assumption for it? *)
-nominal_function chomp\<^sub>1 :: "heap_ty \<Rightarrow> nat \<Rightarrow> heap_ty" where
-  "chomp\<^sub>1 (Sigma x \<tau>\<^sub>1 \<tau>\<^sub>2) n = Choice (Sigma x (chomp\<^sub>1 \<tau>\<^sub>1 n) (chompRef\<^sub>1 \<tau>\<^sub>2 x n))
-                              (Sigma x (Refinement y \<tau>\<^sub>1 (Eq (Len y PktIn) (Num 0))) (chomp\<^sub>1 \<tau>\<^sub>2 n))" |
-  "chomp\<^sub>1 (Choice \<tau>\<^sub>1 \<tau>\<^sub>2) n = Choice (chomp\<^sub>1 \<tau>\<^sub>1 n) (chomp\<^sub>1 \<tau>\<^sub>2 0)" |
-  "chomp\<^sub>1 (Refinement x \<tau> \<phi>) n = Refinement x (chomp\<^sub>1 \<tau> n) (chomp\<^sub>1\<^sub>\<phi> \<phi> x n)" |
-  "chomp\<^sub>1 (Substitution \<tau>\<^sub>1 x \<tau>\<^sub>2) n = Substitution (chomp\<^sub>1 \<tau>\<^sub>1 n) x \<tau>\<^sub>2" |
-  "chomp\<^sub>1 \<tau> _ = \<tau>"
+nominal_function chomp\<^sub>1 :: "heap_ty \<Rightarrow> heap_ty" where
+  "atom y \<sharp> x \<Longrightarrow> chomp\<^sub>1 (Sigma x \<tau>\<^sub>1 \<tau>\<^sub>2) = Choice (Sigma x (chomp\<^sub>1 \<tau>\<^sub>1) (chompRef\<^sub>1 \<tau>\<^sub>2 x))
+                              (Sigma x (Refinement y \<tau>\<^sub>1 (Eq (Len y PktIn) (Num 0))) (chomp\<^sub>1 \<tau>\<^sub>2))" |
+  "chomp\<^sub>1 (Choice \<tau>\<^sub>1 \<tau>\<^sub>2) = Choice (chomp\<^sub>1 \<tau>\<^sub>1) (chomp\<^sub>1 \<tau>\<^sub>2)" |
+  "chomp\<^sub>1 (Refinement x \<tau> \<phi>) = Refinement x (chomp\<^sub>1 \<tau>) (chomp\<^sub>1\<^sub>\<phi> \<phi> x)" |
+  "chomp\<^sub>1 (Substitution \<tau>\<^sub>1 x \<tau>\<^sub>2) = Substitution (chomp\<^sub>1 \<tau>\<^sub>1) x \<tau>\<^sub>2" |
+  "chomp\<^sub>1 \<tau> = \<tau>"
   sorry
 nominal_termination (eqvt)
   by lexicographic_order
 
-nominal_function heapRef\<^sub>1\<^sub>e :: "exp \<Rightarrow> nat \<Rightarrow> var \<Rightarrow> instanc \<Rightarrow> nat"
+nominal_function heapRef\<^sub>1\<^sub>e :: "exp \<Rightarrow> var \<Rightarrow> instanc \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> exp" where
+  "heapRef\<^sub>1\<^sub>e (Bv (b#bv)) x \<iota> sz n = Concat (if b = BitVar
+    then Slice (SlInstance x \<iota>) (sz - n) (sz - n + 1)
+    else Bv [b]) (heapRef\<^sub>1\<^sub>e (Bv bv) x \<iota> sz n)" |
+  "heapRef\<^sub>1\<^sub>e (Plus e\<^sub>1 e\<^sub>2) x \<iota> sz n = Plus (heapRef\<^sub>1\<^sub>e e\<^sub>1 x \<iota> sz n) (heapRef\<^sub>1\<^sub>e e\<^sub>2 x \<iota> sz n)" |
+  "heapRef\<^sub>1\<^sub>e (Concat e\<^sub>1 e\<^sub>2) x \<iota> sz n = Concat (heapRef\<^sub>1\<^sub>e e\<^sub>1 x \<iota> sz n) (heapRef\<^sub>1\<^sub>e e\<^sub>2 x \<iota> sz n)" |
+  "heapRef\<^sub>1\<^sub>e e _ _ _ _ = e"
+  sorry
+nominal_termination (eqvt)
+  by size_change
 
 end
